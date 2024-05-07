8f47bb5b884670bdae0dab463d33734cf59a0d58
TheFireMonster <113089882+TheFireMonster@users.noreply.github.com>
2024-05-07 17:00:21 -0300

    new interface

---
+++ config.py
@@ -0,0 +1,6 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+lang_token = os.getenv('SECRET_TOKEN')
--- main.py
+++ main.py
@@ -1,44 +1,93 @@
+from config import lang_token
+
 import tkinter as tk
 
 from deep_translator import GoogleTranslator, single_detection
 
 def send():
+    label = tk.Label(frame_inner, text='teste', font=app_font, bg="#001900", fg="#009600")
+    label.grid(row=frame_inner.grid_size()[1], column=0, sticky='ew')
+    frame_inner.grid_columnconfigure(0, weight=1)
 
-def on_enter(event):
+    canvas.configure(scrollregion=canvas.bbox("all"))
+
+#def on_enter(event):
   
 
-def translate(text, lang):
-    return GoogleTranslator(source='auto', target=lang).translate(text)
+def translate(tl_text, lang):
+    return GoogleTranslator(source='auto', target=lang).translate(tl_text)
 
-window = tk.Tk()
+root = tk.Tk()
 
-message = tk.Label(window, text="TRECL-AI")
-message.pack()
-text = tk.StringVar()
-text_input = tk.Entry(window, textvariable=text)
-text_input.pack()
+root.geometry("500x500")
+root.minsize(500, 500)
+root.maxsize(500, 500)
 
-window.bind('<Return>', on_enter)
+root['bg'] = "#001900"
 
-window.mainloop()
+app_font = ('Fixedsys Excelsior', 12)
 
-
-api_key = input("Digite sua chave de API: ")
+api_key = lang_token
 test = True
 
-while test:
-  text_test = input("")
-  if str(text_test):
-    lang_test = single_detection(text.get(), api_key)
-    if str(lang_test):
-       if text_test == str(translate("exit", lang_test)) or text_test == str(translate("sair", lang_test)):
-         test = False
-    print(lang_test)
-  else:
-    lang_test = None
+frame_top = tk.Frame(root, bg="#001900")
+frame_top.grid(row=0, column=0, columnspan=2, sticky="ew")
+#frame_middle = tk.Frame(root, bg="#001900")
+#frame_middle.grid(row=1, column=1, sticky='nsew') 
+frame_bottom = tk.Frame(root, bg="#001900")
+frame_bottom.grid(row=2, column=1, columnspan=2, sticky='ew')
 
-user_text = input("Digite o texto a ser traduzido: ")
-user_lang = input("Digite a língua alvo: ")
+scrollbar = tk.Scrollbar(root)
+scrollbar.grid(row=0, column=3, rowspan=3, sticky='ns')
 
-print(f"{translate(user_text, user_lang)}")
+canvas = tk.Canvas(root, bg="red", bd=0, highlightthickness=0, yscrollcommand=scrollbar.set)
+canvas.grid(row=1, column=0, columnspan=3, sticky='nsew')
 
+root.grid_rowconfigure(1, weight=1)
+root.grid_columnconfigure(1, weight=1)
+
+scrollbar.config(command=canvas.yview)
+
+frame_inner = tk.Frame(canvas, bg="yellow")
+canvas.create_window((0, 0), window=frame_inner, anchor='nw', width=canvas.winfo_reqwidth(), height=canvas.winfo_reqheight())
+
+frame_inner.bind(
+    "<Configure>",
+    lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
+)
+
+
+message = tk.Label(frame_top, text="TRECL-AI", font=app_font, bg="#001900", fg="#009600")
+message.pack(expand=True)
+tl_text = tk.StringVar()
+text_input = tk.Entry(frame_bottom, textvariable=tl_text, width=50, font=app_font)
+text_input.pack(side=tk.LEFT, expand=True, fill=tk.X)
+send_button = tk.Button(frame_bottom, text="Send", font=app_font, command=send)
+send_button.pack(side=tk.RIGHT)
+
+#root.bind('<Return>', on_enter)
+
+
+
+
+root.mainloop() 
+#while test:
+#  text_test = input("")
+#  if str(text_test):
+#    lang_test = single_detection(tl_text.get(), api_key)
+#    message_show = tk.Label(root, text=text_test)
+#    message_show.pack
+#  else:
+#    lang_test = None
+
+#user_text = input("Digite o texto a ser traduzido: ")
+#user_lang = input("Digite a língua alvo: ")
+
+#print(f"{translate(user_text, user_lang)}")
+
+
+
+
+# if str(lang_test):
+#       if text_test == str(translate("exit", lang_test)) or text_test == str(translate("sair", lang_test)):
+#         test = False
